0f57d64940da55140c42cd86b4d9cb10bce728f8
Dejan Caric <dejan.caric@gmail.com>
2014-09-16 14:37:31 +0200

    Initial commit

---
+++ MvcDemo/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Web.Mvc;
+
+namespace MvcDemo.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            var viewModel = new MessageViewModel();
+
+            return View(viewModel);
+        }
+
+        [HttpPost]
+        public JsonResult SendMessage(MessageViewModel viewModel)
+        {
+            // the logic goes here
+            return Json(new { isSuccess = true });
+        }
+
+        public class MessageViewModel
+        {
+            [Required]
+            public string Name { get; set; }
+
+            [Required]
+            public string Email { get; set; }
+
+            public string Subject { get; set; }
+
+            [Required]
+            public string Message { get; set; }
+        }
+    }
+}
---
+++ MvcDemo/assets/js/homepage.js
@@ -0,0 +1,50 @@
+﻿var homePage = (function() {
+    // private variables and functions
+
+    function sendMessage() {
+        // collect form data
+        var data = {
+            name: $('#Name').val(),
+            email: $('#Email').val(),
+            subject: $('#Subject').val(),
+            message: $('#Message').val()
+        };
+
+        $.ajax({
+            type: 'POST',
+            url: '/home/sendmessage',
+            data: data
+        }).done(function() {
+            clearFormFields();
+            bootbox.alert('Your message has been sent successfully!');
+        }).fail(function() {
+            bootbox.alert('Something went wrong.');
+        });
+    }
+
+    function clearFormFields() {
+        $('#Name').val('');
+        $('#Email').val('');
+        $('#Subject').val('');
+        $('#Message').val('');
+    }
+
+    var init = function() {
+        $('#btnSend').on('click', function(e) {
+            // stop click event
+            e.preventDefault();
+            // stop event bubbling
+            e.stopPropagation();
+
+            // check if form is valid
+            if ($('#frmMessage').valid()) {
+                sendMessage();
+            }
+        });
+    }
+
+    // public API
+    return {
+        init: init
+    };
+})();
